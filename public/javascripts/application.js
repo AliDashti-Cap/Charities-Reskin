57e085b6ec4f6cb2145d24f78260697b2b0c788c
Ali Dashti <ali.dashti@capgemini.com>
2022-10-11 14:33:50 +0100

    Added Gift Aid Schedule Error Summary Page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,50 +9,65 @@ $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
 
-// Selecting the form
+// Selecting the forms
 const aboutCharityForm = document.getElementById("about-charity-form")
+const aboutOrganisationForm = document.getElementById("about-organisation-form")
 
-// Are you claiming Gift Aid?
+// About the Charity - Are you claiming Gift Aid?
 const claimingGiftAidGroup = document.getElementById("claiming-gift-aid-group")
 const claimingGiftAidErrorMessage = document.getElementById("claiming-gift-aid-error-message")
 const claimingGiftAidSelect = document.getElementById("claiming-gift-aid-select")
 
-// Are you claiming UK tax deducted from other income?
+// About the Charity - Are you claiming UK tax deducted from other income?
 
 const otherIncomeGroup = document.getElementById("other-income-group")
 const otherIncomeErrorMessage = document.getElementById("other-icnome-error-message")
 const otherIncomeSelect = document.getElementById("other-income-select")
 
-// Are you claiming a top up payment for small cash donations under GASDS? These are not your Gift Aid donations.
+// About the Charity - Are you claiming a top up payment for small cash donations under GASDS? These are not your Gift Aid donations.
 
 const topUpCashGasdsGroup = document.getElementById("top-up-cash-gasds-group")
 const topUpCashGasdsErrorMessage = document.getElementById("top-up-cash-gasds-error-message")
 const topUpCashGasdsSelect = document.getElementById("top-up-cash-gasds-select")
 
-// Are you claiming a top up payment under GASDS for donations that were not collected in a community building?
+// About the Charity - Are you claiming a top up payment under GASDS for donations that were not collected in a community building?
 
 const topUpGasdsCommunityBuildingGroup = document.getElementById("top-up-gasds-community-building-group")
 const topUpGasdsCommunityBuildingErrorMessage = document.getElementById("top-up-gasds-community-building-error-message")
 const topUpGasdsCommunityBuildingSelect = document.getElementById("top-up-gasds-community-building-select")
 
-// Are you claiming for donations collected in community buildings? 
+// About the Charity - Are you claiming for donations collected in community buildings? 
 
 const claimingCommunityBuildingGroup = document.getElementById("claiming-community-building-group")
 const claimingCommunityBuildingErrorMessage = document.getElementById("claiming-community-building-error-message")
 const claimingCommunityBuildingSelect = document.getElementById("claiming-community-building-select")
 
-// Are you connected to any other charities or CASCs for the purpose of GASDS?
+// About the Charity - Are you connected to any other charities or CASCs for the purpose of GASDS?
 
 const connectedCharitiesGroup = document.getElementById("connected-charities-group")
 const connectedCharitiesErrorMessage = document.getElementById("connected-charities-error-message")
 const connectedCharitiesSelect = document.getElementById("connected-charities-select")
 
-// Are you making an adjustment to a previous GASDS claim?
+// About the Charity - Are you making an adjustment to a previous GASDS claim?
 
 const adjustmentToGasdsGroup = document.getElementById("adjustment-to-gasds-group")
 const adjustmentToGasdsErrorMessage = document.getElementById("adjustment-to-gasds-error-message")
 const adjustmentToGasdsSelect = document.getElementById("adjustment-to-gasds-select")
 
+// About the Organisation - Name of charity regulator 
+
+const nameCharityRegulatorGroup = document.getElementById("name-charity-regulator-group")
+const nameCharityRegulatorErrorMessage = document.getElementById("name-charity-regulator-error-message")
+const nameCharityRegulatorSelect = document.getElementById("name-charity-regulator-select")
+
+// About the Organisation - Corporate trustee
+
+const corporateTrusteeGroup = document.getElementById("corporate-trustee-group")
+const corporateTrusteeErrorMessage = document.getElementById("corporate-trustee-error-message")
+const corporateTrusteeSelect = document.getElementById("corporate-trustee-select")
+
+
+// About the Charity - Listeners
 aboutCharityForm.addEventListener("submit", function(e) {
   claimingGiftAidGroup.classList.remove("govuk-form-group--error")
   claimingGiftAidErrorMessage.style.display = "none"
@@ -76,8 +91,6 @@ aboutCharityForm.addEventListener("submit", function(e) {
   adjustmentToGasdsErrorMessage.style.display = "none"
   adjustmentToGasdsSelect.classList.remove("govuk-select--error")
 
-
-
   if (claimingGiftAidSelect.value === "default") {
     e.preventDefault()
     claimingGiftAidGroup.classList.add("govuk-form-group--error")
@@ -95,30 +108,54 @@ aboutCharityForm.addEventListener("submit", function(e) {
     topUpCashGasdsGroup.classList.add("govuk-form-group--error")
     topUpCashGasdsErrorMessage.style.display = "block"
     topUpCashGasdsSelect.classList.add("govuk-select--error")
-}
-if (topUpGasdsCommunityBuildingSelect.value === "default") {
-  e.preventDefault()
-  topUpGasdsCommunityBuildingGroup.classList.add("govuk-form-group--error")
-  topUpGasdsCommunityBuildingErrorMessage.style.display = "block"
-  topUpGasdsCommunityBuildingSelect.classList.add("govuk-select--error")
-}
-if (claimingCommunityBuildingSelect.value === "default") {
-  e.preventDefault()
-  claimingCommunityBuildingGroup.classList.add("govuk-form-group--error")
-  claimingCommunityBuildingErrorMessage.style.display = "block"
-  claimingCommunityBuildingSelect.classList.add("govuk-select--error")
-}
-if (connectedCharitiesSelect.value === "default") {
-  e.preventDefault()
-  connectedCharitiesGroup.classList.add("govuk-form-group--error")
-  connectedCharitiesErrorMessage.style.display = "block"
-  connectedCharitiesSelect.classList.add("govuk-select--error")
-}
-if (adjustmentToGasdsSelect.value === "default") {
-  e.preventDefault()
-  adjustmentToGasdsGroup.classList.add("govuk-form-group--error")
-  adjustmentToGasdsErrorMessage.style.display = "block"
-  adjustmentToGasdsSelect.classList.add("govuk-select--error")
-}
+  }
+  if (topUpGasdsCommunityBuildingSelect.value === "default") {
+    e.preventDefault()
+    topUpGasdsCommunityBuildingGroup.classList.add("govuk-form-group--error")
+    topUpGasdsCommunityBuildingErrorMessage.style.display = "block"
+    topUpGasdsCommunityBuildingSelect.classList.add("govuk-select--error")
+  }
+  if (claimingCommunityBuildingSelect.value === "default") {
+    e.preventDefault()
+    claimingCommunityBuildingGroup.classList.add("govuk-form-group--error")
+    claimingCommunityBuildingErrorMessage.style.display = "block"
+    claimingCommunityBuildingSelect.classList.add("govuk-select--error")
+  }
+  if (connectedCharitiesSelect.value === "default") {
+    e.preventDefault()
+    connectedCharitiesGroup.classList.add("govuk-form-group--error")
+    connectedCharitiesErrorMessage.style.display = "block"
+    connectedCharitiesSelect.classList.add("govuk-select--error")
+  }
+  if (adjustmentToGasdsSelect.value === "default") {
+    e.preventDefault()
+    adjustmentToGasdsGroup.classList.add("govuk-form-group--error")
+    adjustmentToGasdsErrorMessage.style.display = "block"
+    adjustmentToGasdsSelect.classList.add("govuk-select--error")
+  }
 })
 
+// About the Organisation - Listeners
+aboutOrganisationForm.addEventListener("submit", function(e) {
+  nameCharityRegulatorGroup.classList.remove("govuk-form-group--error")
+  nameCharityRegulatorErrorMessage.style.display = "none"
+  nameCharityRegulatorSelect.classList.remove("govuk-select--error")
+  corporateTrusteeGroup.classList.remove("govuk-form-group--error")
+  corporateTrusteeErrorMessage.style.display = "none"
+  corporateTrusteeSelect.classList.remove("govuk-select--error")
+
+
+  if (nameCharityRegulatorSelect.value === "default") {
+    e.preventDefault()
+    nameCharityRegulatorGroup.classList.add("govuk-form-group--error")
+    nameCharityRegulatorErrorMessage.style.display = "block"
+    nameCharityRegulatorSelect.classList.add("govuk-select--error")
+  }
+
+  if (corporateTrusteeSelect.value === "default") {
+    e.preventDefault()
+    corporateTrusteeGroup.classList.add("govuk-form-group--error")
+    corporateTrusteeErrorMessage.style.display = "block"
+    corporateTrusteeSelect.classList.add("govuk-select--error")
+  }
+})
